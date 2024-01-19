bbbdc3b18eaa09926f858eb047955cf710eb7cc1
stoodnia <k.studniarek.414@studms.ug.edu.pl>
2024-01-19 10:07:23 +0100

    db config=

--- BACKEND/src/app.js
+++ BACKEND/src/app.js
@@ -8,8 +8,20 @@ const app = express();
 // logger setup
 const pinoHttp = require("pino-http");
 const logger = require("./config/logger.config.js");
+const { driver } = require("neo4j-driver");
 app.use(pinoHttp({ logger }));
 
 // req, res parsing
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// init database
+const initDatabase = require("./config/db.config.js");
+initDatabase.configNeo4j();
+
+// destructor
+process.on("SIGINT", () => {
+  session.close();
+  logger.info("Bye bye!");
+  process.exit();
+});
--- BACKEND/src/config/db.config.js
+++ BACKEND/src/config/db.config.js
@@ -1,1 +1,41 @@
 const neo4j = require("neo4j-driver");
+const logger = require("./logger.config.js");
+
+let neo4jDriver;
+
+const configNeo4j = () => {
+  try {
+    const { DATABASE_URL, DATABASE_USERNAME, DATABASE_PASSWORD } = process.env;
+    if (!DATABASE_URL || !DATABASE_USERNAME || !DATABASE_PASSWORD) {
+      logger.error(
+        "Missing .env variables(DATABASE_URL, DATABASE_USERNAME, DATABASE_PASSWORD)"
+      );
+      throw new Error(
+        "Missing .env variables(DATABASE_URL, DATABASE_USERNAME, DATABASE_PASSWORD)"
+      );
+    }
+    const config = {
+      maxTransactionRetryTime: 30000,
+      maxConnectionPoolSize: 50,
+      maxConnectionLIFEtime: 100000,
+    };
+    const driver = neo4j.driver(
+      DATABASE_URL,
+      neo4j.auth.basic(DATABASE_USERNAME, DATABASE_PASSWORD),
+      config
+    );
+    neo4jDriver = driver;
+    logger.info("Neo4j driver created, connected to database");
+  } catch (error) {
+    logger.error("Neo4j error: ", error);
+  }
+};
+
+const neo4j = () => {
+  return neo4jDriver;
+};
+
+module.exports = {
+  configNeo4j,
+  neo4j,
+};
