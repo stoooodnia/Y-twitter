ee103280a88f9264fa09d9dd07fb350854cd9227
stoodnia <k.studniarek.414@studms.ug.edu.pl>
2024-01-29 14:33:14 +0100

    cleared navbar

--- FRONTEND/src/views/NavBarView.vue
+++ FRONTEND/src/views/NavBarView.vue
@@ -19,8 +19,8 @@
         </div>
         <nav class="mt-10 px-2 space-y-1">
           <RouterLink
-            to="/"
-            :class="{ 'bg-gray-900 text-white': page === 'home', 'text-gray-300 hover:text-white hover:bg-gray-700': page !== 'home'}"
+            to="/wall"
+            :class="{ 'bg-gray-900 text-white': page === 'wall', 'text-gray-300 hover:text-white hover:bg-gray-700': page !== 'wall'}"
             class=" flex items-center px-2 py-2 text-base leading-6 font-medium rounded-md text-white "
           >
             <svg
@@ -38,76 +38,9 @@
               <path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
               <polyline points="9 22 9 12 15 12 15 22"></polyline>
             </svg>
-            Home
-          </RouterLink>
-  
-          <RouterLink
-            to="/wall"
-            :class="{ 'bg-gray-900 text-white': page === 'hom', 'text-gray-300 hover:text-white hover:bg-gray-700': page !== 'hom'}"
-            class="flex items-center px-2 py-2 text-base leading-6 font-medium rounded-md "
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              class="mr-4 h-6 w-6 text-gray-400 hover:text-gray-300  bg-transparent"
-            >
-              <circle cx="11" cy="11" r="8"></circle>
-              <path d="m21 21-4.3-4.3"></path>
-            </svg>
             Wall
           </RouterLink>
-  
-          <RouterLink
-            to="/register"
-            :class="{ 'bg-gray-900 text-white': page === 'register', 'text-gray-300 hover:text-white hover:bg-gray-700': page !== 'register'}"
-            class=" flex items-center px-2 py-2 text-base leading-6 font-medium rounded-md "          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              class="mr-4 h-6 w-6 text-gray-400 -hover:text-gray-300  bg-transparent"
-            >
-              <path d="M6 8a6 6 0 0 1 12 0c0 7 3 9 3 9H3s3-2 3-9"></path>
-              <path d="M10.3 21a1.94 1.94 0 0 0 3.4 0"></path>
-            </svg>
-            Register
-          </RouterLink>
-  
-          <RouterLink
-            to="/login"
-            :class="{ 'bg-gray-900 text-white': page === 'login', 'text-gray-300 hover:text-white hover:bg-gray-700': page !== 'login'}"
-            class=" flex items-center px-2 py-2 text-base leading-6 font-medium rounded-md "          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              class="mr-4 h-6 w-6 text-gray-400 -hover:text-gray-300  bg-transparent"
-            >
-              <rect width="20" height="16" x="2" y="4" rx="2"></rect>
-              <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
-            </svg>
-            Login
-          </RouterLink>
-  
+
           <RouterLink
             to="/"
             :class="{ 'bg-gray-900 text-white': page === 'hoe', 'text-gray-300 hover:text-white hover:bg-gray-700': page !== 'ome'}"
@@ -126,7 +59,7 @@
         >
           <path d="m19 21-7-4-7 4V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2v16z"></path>
         </svg>
-            -
+            Notifications
           </RouterLink>
   
           <RouterLink
