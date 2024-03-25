c9bb76c6e9daf22096a7256e50150dce51ccef23
stoodnia <k.studniarek.414@studms.ug.edu.pl>
2024-03-25 11:25:05 +0100

    changed navbar sb dir

--- FRONTEND/src/router/index.js
+++ FRONTEND/src/router/index.js
@@ -3,7 +3,7 @@ import { healthGuard } from "@/guard/healthGuard";
 import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL), // todo: what is it??
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/register",
